ff7c051b874f1b12d6576df6325cf590c03cc813
Joël Kuijten <jkuijten@xebia.com>
2017-03-24 13:35:06 +0100

    fixed merge conflicts

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,24 @@
 const vimeo = document.querySelector('#vimeo');
 
-var length = 0
-const n_slides = 57
+let length = 0
+let numberOfSslides;
 
 var player = new Vimeo.Player(vimeo);
 player.getDuration().then(function(duration) {
   console.log('got duration', duration)
   length = duration
-  const images = document.querySelectorAll('.slide-container img')
-  for (var i = 0; i < images.length; i++) {
-    const j = i;
-    images[i].addEventListener('click', function(evt) {
-      console.log('Clicked', j)
-      seekToSlide(j);
-    })
-  }
+  Array.from(document.querySelectorAll('.slide-container img')).forEach((el, index) => {
+     el.addEventListener('click', function(evt) {
+      console.log('Clicked', index);
+      seekToSlide(index);
+      numberOfSslides = index;
+    });
+  })
 })
 player.play()
 
 function seekToSlide(n) {
-  var target = length * n / n_slides
-  console.log('seeking to', target)
+  var target = length * n / numberOfSslides;
+  console.log('seeking to', target);
   player.setCurrentTime(target)
 }
